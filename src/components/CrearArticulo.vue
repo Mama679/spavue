ff119c124a2aa038385d49749bef3b2f877f8e99
mama679 <ingmariomanotas@hotmail.com>
2023-07-11 00:03:38 -0500

    Estructura Articulo

---
+++ src/components/CrearArticulo.vue
@@ -0,0 +1,47 @@
+<template>
+     <v-container>
+        <v-row class="text-center">    
+            <v-col class="mb-4">
+                <h1 class="display-2 font-weight-bold mb-3">Insertar Articulos</h1>       
+            </v-col>     
+        </v-row>
+        <v-row>
+            <v-col>
+                <form v-on:submit.prevent="guardarArticulo()">
+                    <v-text-field v-model="articulo.descripcion"
+                            label="Descripción"
+                            outlined
+                            required   
+                    >
+                    </v-text-field>
+                    <v-text-field
+                        v-model="articulo.precio"        
+                        label="Precio"
+                        type="number"                
+                        prefix="$"
+                        outlined
+                        required        
+                    >
+                    </v-text-field>
+                    <v-text-field
+                        v-model="articulo.stock"        
+                        label="Stock"
+                        type="number"
+                        outlined
+                        required        
+                    >
+                    </v-text-field>
+                    <v-card-actions>
+                        <v-btn color="warning" class="mr-4" type="submit">Guardar</v-btn>      
+                    </v-card-actions>
+                </form>
+            </v-col>
+        </v-row>
+     </v-container>
+</template>
+
+<script>
+export default {
+    name:'crearArticulo'
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify'
+import vuetify from './plugins/vuetify';
+
+//Importar componentes creados
+import inicio from './components/Inicio';
+import listarArticulo from './components/ListarArticulo';
+import crearArticulo from './components/CrearArticulo';
+import editarArticulo from './components/EditarArticulo';
+import contacto from './components/Contacto';
+
+Vue.component('inicio',inicio);
+Vue.component('listarArticulo',listarArticulo);
+Vue.component('crearArticulo',crearArticulo);
+Vue.component('editarArticulo',editarArticulo);
+Vue.component('contacto',contacto);
 
 Vue.config.productionTip = false
 
